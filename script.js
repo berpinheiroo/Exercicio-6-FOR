f075e1acb02dd6b8aeeb4826092a4a20bdfd38ef
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-29 20:46:57 -0300

    Mudando de repositorio

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Agora faça com que o algoritmo do exercício anterior exiba a mesma contagem mas
+na ordem inversa. */
+
+
+var valor = 50
+
+for (i = 0; i <= valor; i++) {
+   console.log(i)
+   i = i + 4
+    
+}
